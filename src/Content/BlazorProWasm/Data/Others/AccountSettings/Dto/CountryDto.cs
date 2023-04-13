e480655b598f38a15483a476427c87fd450c36a1
capdiem <capdiem@live.com>
2023-04-13 17:14:33 +0800

    🆕 feat(BlazorProWasm): add blazor pro wasm template (#21)

---
+++ src/Content/BlazorProWasm/Data/Others/AccountSettings/Dto/CountryDto.cs
@@ -0,0 +1,15 @@
+﻿namespace BlazorProWasm.Data.Others.AccountSettings.Dto
+{
+    public class CountryDto
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public CountryDto(string id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+}
