2e8d5f5c06e7df3b2abb58a81daf87cd67714400
capdiem <capdiem@live.com>
2023-04-13 16:02:23 +0800

    🆕 feat(BlazorProServer): Add blazor pro server template

---
+++ src/Content/BlazorProServer/Global/Config/NavigationStyles.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorProServer.Global.Config
+{
+    public static class NavigationStyles
+    {
+        public const string Flat = "Flat";
+        public const string Rounded = "Rounded";
+    }
+}
