b1cb24fe49658734610cec0b01936f919e7cd383
capdiem <capdiem@live.com>
2023-04-19 17:51:09 +0800

    Rename RCL to Rcl

--- src/Content/BlazorMaui/BlazorMaui.Client/Program.cs
+++ src/Content/BlazorMaui/BlazorMaui.Client/Program.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using BlazorMaui.RCL;
+using BlazorMaui.Rcl;
 using BlazorMaui.Shared;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
