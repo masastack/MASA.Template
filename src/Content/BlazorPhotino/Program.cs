bc85c845e99c67a28fcfdad231fca2fa85e326a3
token <239573049@qq.com>
2023-05-10 14:58:20 +0800

    增加Photino模板
    Signed-off-by: token <239573049@qq.com>

---
+++ src/Content/BlazorPhotino/Program.cs
@@ -0,0 +1,26 @@
+﻿using BlazorPhotino;
+using Microsoft.Extensions.DependencyInjection;
+using Photino.Blazor;
+
+internal class Program
+{
+    [STAThread]
+    private static void Main(string[] args)
+    {
+        var appBuilder = PhotinoBlazorAppBuilder.CreateDefault(args);
+
+        appBuilder.RootComponents.Add<App>("#app");
+        appBuilder.Services.AddMasaBlazor();
+
+        var app = appBuilder.Build();
+
+        app.MainWindow
+            .SetTitle("Photino Blazor Sample");
+
+        AppDomain.CurrentDomain.UnhandledException += (sender, error) =>
+        {
+        };
+
+        app.Run();
+    }
+}
