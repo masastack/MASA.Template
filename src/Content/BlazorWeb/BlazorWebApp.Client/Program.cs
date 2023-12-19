b6364e349252ff2f93942f80f29970c07da7c38d
capdiem <capdiem@live.com>
2023-12-19 11:11:31 +0800

    feat(blazor): Update blazor web app template (#69)
    * 🚧 feat: wip
    
    * done

--- src/Content/BlazorWeb/BlazorWebApp.Client/Program.cs
+++ src/Content/BlazorWeb/BlazorWebApp.Client/Program.cs
@@ -5,7 +5,15 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 #if (!InteractiveAtRoot)
 builder.Services.AddMasaBlazor(options =>
 {
-    options.ConfigureSSR(_ => {});
+    #if (SampleContent)
+    options.ConfigureSsr(ssr =>
+    {
+        ssr.Left = 256;
+        ssr.Top = 64;
+    });
+    #else
+    options.ConfigureSsr();
+    #endif
 });
 #else
 builder.Services.AddMasaBlazor();
