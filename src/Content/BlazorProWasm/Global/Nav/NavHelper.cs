dd129a5fb06f995714d303fa108f414bacf89a0e
capdiem <capdiem@live.com>
2024-07-03 14:44:07 +0800

    Update Pro template (#79)

--- src/Content/BlazorProWasm/Global/Nav/NavHelper.cs
+++ src/Content/BlazorProWasm/Global/Nav/NavHelper.cs
@@ -30,12 +30,12 @@ public class NavHelper
             {
                 nav.Children = nav.Children.Where(c => c.Hide is false).ToArray();
 
-                nav.Children.ForEach(child =>
+                foreach (var child in nav.Children)
                 {
                     child.ParentId = nav.Id;
                     child.FullTitle = $"{nav.Title} {child.Title}";
                     child.ParentIcon = nav.Icon;
-                });
+                }
             }
         });
 
@@ -45,7 +45,7 @@ public class NavHelper
             if (nav.Children is not null) SameLevelNavs.AddRange(nav.Children);
         });
 
-        SameLevelNavs.Where(nav => nav.Href is not null).ForEach(nav =>
+        foreach (var nav in SameLevelNavs.Where(nav => nav.Href is not null))
         {
             // The following path will not open a new tab
             if (nav.Href is "app/user/view" or "app/user/edit" or "app/ecommerce/details")
@@ -54,7 +54,7 @@ public class NavHelper
             }
 
             PageTabItems.Add(new(nav.Title, nav.Href, nav.ParentIcon));
-        });
+        }
     }
 
     public void NavigateTo(NavModel nav)
